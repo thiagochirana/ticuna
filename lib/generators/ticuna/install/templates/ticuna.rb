de693d9e07381d435fab8dfd9aa46ece2e53d0e0
Chirana <engsw.thiago@gmail.com>
2025-10-03 02:19:09 -0300

    feat: improve generators

---
+++ lib/generators/ticuna/install/templates/ticuna.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Ticuna.config do |c|
+  # Configure your API Keys here
+  # c.openai_token   = ENV["OPENAI_API_KEY"]
+  # c.anthropic_token = ENV["ANTHROPIC_API_KEY"]
+  # c.deepseek_token  = ENV["DEEPSEEK_API_KEY"]
+  # c.mistral_token   = ENV["MISTRAL_API_KEY"]
+end
--- lib/ticuna.rb
+++ lib/ticuna.rb
@@ -3,6 +3,8 @@
 require_relative "ticuna/version"
 require_relative "ticuna/llm"
 require_relative "ticuna/tool"
+# require "generators/ticuna/install/install_generator"
+
 
 module Ticuna
   class Error < StandardError; end
